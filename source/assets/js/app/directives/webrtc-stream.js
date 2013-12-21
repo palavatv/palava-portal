604fca9aa35bd41036ff5cbabe9da537bc4c74dc
Thammi <thammi@chaossource.net>
2013-12-21 20:29:39 +0100

    Watch stream and not peer for change

--- source/assets/js/app/directives/webrtc-stream.js
+++ source/assets/js/app/directives/webrtc-stream.js
@@ -7,12 +7,8 @@ angular.module('palava-portal').directive('webrtcStream', function(){
       },
       link: function($scope, $elem, $attrs){
         palava.browser.registerFullscreen($elem, 'dblclick');
-        $scope.$watch('peer', function(val){
-          if(val) {
-            palava.browser.attachMediaStream($elem, val.getStream());
-          } else {
-            palava.browser.attachMediaStream($elem, null);
-          }
+        $scope.$watch('peer.getStream()', function(val){
+          palava.browser.attachMediaStream($elem, val);
         });
       },
       template: '<video autoplay muted class="plv-video-like" ng-class="{\'plv-local-video\': peer.isLocal() }" />'
