cba39730e288230024063931e9e63dd688e29980
Jan Lelis <mail@janlelis.de>
2017-05-06 10:42:05 +0200

    Tweaks for start page to get old behavior

--- source/assets/js/app/components/start_page.es6.jsx
+++ source/assets/js/app/components/start_page.es6.jsx
@@ -8,27 +8,25 @@ class StartPage extends React.Component {
   }
 
   render() {
-    return(
-      <div>
-        <div id="wrap">
-          <div className="homepage earth">
-            <div className="content container">
-              <img src="/assets/images/palava-papagei.svg" alt="palava.tv" />
-              <form className="join-room">
-                <div className="hide nosupport">
-                  <p><strong>Sorry!</strong> Your web browser is not compatible with the latest technologies for video communication on the web. In order to use <a href="/info/how">palava</a>, you will need to install a web browser that supports WebRTC:</p>
-                  <SupportList/>
+    return <div>
+      <div id="wrap">
+        <div className="homepage earth">
+          <div className="content container">
+            <img src="/assets/images/palava-papagei.svg" alt="palava.tv" />
+            <form className="join-room" onSubmit={this.handleClick}>
+              <div className="hide nosupport">
+                <p><strong>Sorry!</strong> Your web browser is not compatible with the latest technologies for video communication on the web. In order to use <a href="/info/how">palava</a>, you will need to install a web browser that supports WebRTC:</p>
+                <SupportList/>
+              </div>
+              <div className="join-room-fields">
+                <div className="input-append">
+                  <input type="text" maxLength="50" name="room_id" id="room_id" className="input-large room-id" placeholder="Conference Name" value={this.state.roomId} onChange={this.handleChange}/>
+                  <button className="btn btn-primary btn-go" onClick={this.handleClick}>▸</button>
                 </div>
-                <div className="join-room-fields">
-                  <div className="input-append">
-                    <input type="text" maxLength="50" name="room_id" id="room_id" className="input-large room-id" placeholder="Conference Name" value={this.state.roomId} onChange={this.handleChange}/>
-                    <button className="btn btn-primary btn-go" onClick={this.handleClick}>▸</button>
-                  </div>
-                  <span className="button-separator">or</span>
-                  <button className="btn btn-warning btn-private" onClick={this.handleClickPrivate}>Private</button>
-                </div>
-              </form>
-            </div>
+                <span className="button-separator">or</span>
+                <button className="btn btn-warning btn-private" onClick={this.handleClickPrivate}>Private</button>
+              </div>
+            </form>
           </div>
         </div>
         <GitHubRibbon/>
@@ -43,7 +41,11 @@ class StartPage extends React.Component {
 
   handleClick(e) {
     e.preventDefault()
-    ReactRouter.browserHistory.push(this.state.roomId)
+    if(this.state.roomId){
+      ReactRouter.browserHistory.push(this.state.roomId)
+    } else {
+      ReactRouter.browserHistory.push(guid())
+    }
   }
 
   handleClickPrivate(e) {
