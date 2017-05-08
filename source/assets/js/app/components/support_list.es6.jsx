6a029dd1e71c8a2552851ad9729b0917a9cfb558
Jan Lelis <mail@janlelis.de>
2017-05-09 01:39:08 +0200

    Nicer support list

--- source/assets/js/app/components/support_list.es6.jsx
+++ source/assets/js/app/components/support_list.es6.jsx
@@ -1,9 +1,26 @@
 const SupportList = (props) => {
   return (
     <ul className="unstyled support-list">
-      <li><a href="https://google.com/chrome"><img src="/assets/images/chrome.png" />Google Chrome</a></li>
-      <li><a href="https://www.mozilla.org/firefox"><img src="/assets/images/firefox.png" />Mozilla Firefox</a></li>
-      <li><a href="https://www.opera.com"><img src="/assets/images/opera.png" />Opera</a></li>
+      <li>
+        <a href="https://google.com/chrome">
+          <img src="/assets/images/chrome.png" />
+          <span>Google Chrome</span>
+        </a>
+      </li>
+
+      <li>
+        <a href="https://www.mozilla.org/firefox">
+          <img src="/assets/images/firefox.png" />
+          <span>Mozilla Firefox</span>
+        </a>
+      </li>
+
+      <li>
+        <a href="https://www.opera.com">
+          <img src="/assets/images/opera.png" />
+          <span>Opera Browser</span>
+        </a>
+      </li>
     </ul>
   )
 }
