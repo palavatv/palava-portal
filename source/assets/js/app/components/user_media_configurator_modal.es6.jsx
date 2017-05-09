e7b115fc234fce4eb2a45d7724b20b9470261f23
Jan Lelis <mail@janlelis.de>
2017-05-09 16:31:17 +0200

    Improve buttons and add info link to user media configurator

--- source/assets/js/app/components/user_media_configurator_modal.es6.jsx
+++ source/assets/js/app/components/user_media_configurator_modal.es6.jsx
@@ -1,15 +1,16 @@
 const UserMediaConfiguratorModal = (props) => {
   return <div className="modal hide fade modal-center" id="modal-user-media-configurator" data-backdrop="static">
     <div className="modal-header">
-      <h3>You are about to join a <a href="/info/how">palava</a> conference!</h3>
+      <h3>You are about to join a palava conference!</h3>
     </div>
     <div className="modal-body">
-      <p>Please choose which streams you want to send</p>
+      <p>Please choose which media streams you want to send to other participants</p>
       <ul className="user-media-buttons">
-        <li><button onClick={() => props.initConferenceFn({video: true, audio:true})} id="user-media-video-audio" className="btn btn-primary" autoFocus>Video &amp; Audio</button></li>
-        <li><button onClick={() => props.initConferenceFn({video: true, audio:false})} id="user-media-video" className="btn btn-info">Video only</button></li>
-        <li><button onClick={() => props.initConferenceFn({video: false, audio:true})} id="user-media-audio" className="btn btn-info">Audio only</button></li>
+        <li><button onClick={() => props.initConferenceFn({video: true, audio:true})} id="user-media-video-audio" className="btn btn-primary">Video &amp; Audio</button></li>
+        <li><button onClick={() => props.initConferenceFn({video: true, audio:false})} id="user-media-video" className="btn btn-info">Only Video</button></li>
+        <li><button onClick={() => props.initConferenceFn({video: false, audio:true})} id="user-media-audio" className="btn btn-info">Only Audio</button></li>
       </ul>
+      <p><Link to="/info/how">click here to learn about palava</Link></p>
     </div>
   </div>
 }
