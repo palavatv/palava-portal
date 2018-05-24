341bf207c10de69a67497d96177133e643c06de1
Jan Lelis <mail@janlelis.de>
2018-05-24 21:00:53 +0200

    Add privacy policy to prompt

--- source/assets/js/app/components/user_media_configurator_modal.es6.jsx
+++ source/assets/js/app/components/user_media_configurator_modal.es6.jsx
@@ -4,7 +4,8 @@ const UserMediaConfiguratorModal = (props) => {
       <h3>You are about to join a palava conference!</h3>
     </div>
     <div className="modal-body">
-      <p>Please choose which media streams you want to send to other participants</p>
+      <p><strong>Please note:</strong> This will join or create a video chat room with others. For technical reasons, your IP address and other personal data is sent to all other participants via our server. See our <a href="/info/privacy">privacy policy</a> for more details. </p>
+      <p>Choose which media streams you want to send to other participants<br/>and consent to our privacy policy:</p>
       <ul className="user-media-buttons">
         <li><button onClick={() => props.initConferenceFn({video: true, audio:true})} id="user-media-video-audio" className="btn btn-primary">Video &amp; Audio</button></li>
         <li><button onClick={() => props.initConferenceFn({video: true, audio:false})} id="user-media-video" className="btn btn-info">Only Video</button></li>
